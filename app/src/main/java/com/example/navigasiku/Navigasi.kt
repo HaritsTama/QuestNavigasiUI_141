919ff9c048ceb948f8a7593046ab8b595665f3ef
HaritsTama <nayotama1818@gmail.com>
2025-11-06 16:56:10 +0700

    Menambahkan navigasi antar halaman FormIsian dan TampilData pada function DataApp di dalam Navigasi.kt

--- app/src/main/java/com/example/navigasiku/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/Navigasi.kt
@@ -1,11 +1,17 @@
 package com.example.navigasiku
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavUri
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.navigasiku.view.FormIsian
+import com.example.navigasiku.view.TampilData
 
 enum class Navigasi {
     Formulirku,
@@ -18,6 +24,32 @@ fun DataApp(
     modifier: Modifier
 ){
     Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
 
+            modifier = Modifier.padding(isiRuang)) {
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian(
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulirku(navController)
+                    }
+                )
+            }
+        }
     }
+}
+
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+) {
+    navController.popBackStack(Navigasi.Formulirku.name,
+        inclusive = false)
 }
