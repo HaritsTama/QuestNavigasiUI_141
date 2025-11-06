3642e80b724a48165027b1e8f173c6175f0ce377
HaritsTama <nayotama1818@gmail.com>
2025-11-06 17:15:20 +0700

    Update Main Activity

--- app/src/main/java/com/example/navigasiku/MainActivity.kt
+++ app/src/main/java/com/example/navigasiku/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.navigasiku
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,34 +15,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.navigasiku.ui.theme.NavigasiKuTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NavigasiKuTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    DataApp(modifier = Modifier)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavigasiKuTheme {
-        Greeting("Android")
     }
 }
